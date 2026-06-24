91b677272f39bc0cc1fb97ba1f096b6eb493302f
Matias Freire <matiaspfreire@gmail.com>
2026-06-24 00:00:46 -0400

    added SEO Stuff

--- app/seo.ts
+++ app/seo.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
   name: "Stevens ColorStack",
   title: "Stevens ColorStack - Black and Latinx CS Community at Stevens",
+  url: "https://stevenscolorstack.org",
   description:
     "ColorStack at Stevens Institute of Technology supports Black and Latinx computer science students with mentorship, career resources, community, and technical career programming.",
   email: "colorstackstevens@gmail.com",
@@ -15,14 +16,8 @@ export const siteConfig = {
   },
 };
 
-const rawSiteUrl = process.env.NEXT_PUBLIC_SITE_URL;
-
-export const siteUrl = rawSiteUrl
-  ? new URL(/^https?:\/\//.test(rawSiteUrl) ? rawSiteUrl : `https://${rawSiteUrl}`)
-  : undefined;
+export const siteUrl = new URL(siteConfig.url);
 
 export function absoluteUrl(path = "/") {
-  if (!siteUrl) return path;
-
   return new URL(path, siteUrl).toString();
 }
